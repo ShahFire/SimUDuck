fa2098592e344ac61226fcd480162369c9e0ae79
ShahFire <76734415+ShahFire@users.noreply.github.com>
2022-01-15 21:25:07 +0300

    Add files via upload

---
+++ LR2_1/Duck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2_1
+{
+    abstract class Duck
+    {
+        public string Quack()
+        {
+            return "Quack " + this.GetType();
+        }
+        public string Swim()
+        {
+            return "Swim " + this.GetType();
+        }
+        public abstract string Display();
+    }
+
+    class MallardDuck : Duck
+    {
+        public override string Display()
+        {
+            return "Display " + this.GetType();
+        }
+    }
+
+    class RedheadDuck : Duck
+    {
+        public override string Display()
+        {
+            return "Display " + this.GetType();
+        }
+    }
+}
---
+++ LR2_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck Bob = new MallardDuck();
+            Duck Mac = new RedheadDuck();
+
+            Duck[] ducks = { Bob, Mac };
+
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine(ducks[i].Quack());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Display());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
